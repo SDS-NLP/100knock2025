d146233c0b6b91ec41b13af34d3e5036bb7302a0
hytdaiki <daikihayata20040428@gmail.com>
2025-04-19 04:36:12 +0900

    hello

---
+++ daiki/chapter01/knock03.py
@@ -0,0 +1,12 @@
+s = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
+a = list(s.split())
+numbers = []
+for i in range(len(a)):
+    if "," in a[i]:
+        numbers.append(len(a[i].strip(",")))
+    elif "." in a[i]:
+        numbers.append(len(a[i].strip(".")))
+    else:
+        numbers.append(len(a[i]))
+for ele in numbers:
+    print(ele)
