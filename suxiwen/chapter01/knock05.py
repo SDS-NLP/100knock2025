736628007fdac0228970ff79c4b1aa02385327be
SUXIWEN02 <suxiwen021030@gmail.com>
2025-04-20 20:14:52 +0800

    add knock00-09.py files

---
+++ suxiwen/chapter01/knock05.py
@@ -0,0 +1,25 @@
+def create_bigram(s):
+    return [s[i:i + 2] for i in range(len(s) - 1)]
+
+
+str_x = "paraparaparadise"
+str_y = "paragraph"
+
+
+X = set(create_bigram(str_x))
+Y = set(create_bigram(str_y))
+
+union_xy = X | Y  
+intersection_xy = X & Y 
+difference_xy = X - Y  
+
+
+se_in_x ='se' in X
+se_in_y ='se' in Y
+
+
+print("X と Y の和集合:", union_xy)
+print("X と Y の積集合:", intersection_xy)
+print("X と Y の差集合:", difference_xy)
+print("'se' が X に含まれるか:", se_in_x)
+print("'se' が Y に含まれるか:", se_in_y)
---
+++ suxiwen/chapter01/knock09.py
@@ -0,0 +1,21 @@
+import random
+
+def typoglycemia(word):
+    if len(word) <= 4:
+        return word
+    chars = list(word)
+    first = chars[0]
+    last = chars[-1]
+    middle = chars[1:-1]
+    random.shuffle(middle)
+    return first + ''.join(middle) + last
+
+sentence = "I couldn’t believe that I could actually understand what I was reading : the phenomenal power of the human mind ."
+words = sentence.split()
+processed_words = [typoglycemia(word) for word in words]
+result = ' '.join(processed_words)
+
+print("元の文：")
+print(sentence)
+print("\n処理後の文：")
+print(result)
