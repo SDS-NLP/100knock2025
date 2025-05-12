e5a5a6160b1a0c3b6f6c87ca73bb171775e5332b
hiroaki-1-1-0 <contact.hiroaki.110@gmail.com>
2025-05-12 16:36:41 +0900

    tmp commit: 25-29

--- hiro/chapter03/knock25.py
+++ hiro/chapter03/knock25.py
@@ -1,23 +1,19 @@
+# 分からん
+
 from knock20 import uk_article_txt
 import re
-import collections
 
-pattern = "(={2,4}.*?={2,4})"
-result = re.findall(pattern, uk_article_txt)
-section = {}
-
-for text in result:
-    # text
-    c1 = collections.Counter(text)
-    c2 = int(c1['=']/2) - 1
-    text = text.replace('=', '')
-    section[text] = c2
-
-print(section)
+pattern = '基礎情報(.*?\<references/\>)'
+uk_bases = re.findall(pattern, uk_article_txt)
+uk_bases[0] += "\\n"
+pattern = '(?<=\\\\n\|)(.*?) *= *(.*?)(?=\\\\n)'
+uk_bases_2 = re.findall(pattern, uk_bases[0])
+inf_dic = {}
+for i, j in uk_bases_2:
+  inf_dic[i] = j
+print(inf_dic)
 
 """
-={2,4}: これは再び、2回以上4回以下の = に一致します。
-( ... ): これはキャプチャグループを作成します。この部分に一致した文字列は後で参照することができます。
 .: 任意の1文字に一致します。改行文字を除くすべての文字が対象です。
 *: 直前の文字（この場合は.）が0回以上繰り返されることを意味します。つまり、任意の文字が0回以上続くことを許可します。
 ?: 直前の量指定子（この場合は*）を最短一致に変更します。通常、*はできるだけ多くの文字に一致しようとしますが、?を付けることで、最短の一致を探すようになります。
