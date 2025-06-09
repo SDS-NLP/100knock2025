05fd583c66112ba6ab58693a8de24952b9483d03
shoitsu <itsukihidaka404@gmail.com>
2025-06-09 13:33:09 +0900

    submit ch06 kanock55-59

--- itsukihidaka/chapter06/knock50.py
+++ itsukihidaka/chapter06/knock50.py
@@ -1,6 +1,7 @@
-# Google Newsデータセット（約1,000億単語）での学習済み単語ベクトル（300万単語・フレーズ、300次元）をダウンロードし、”United States”の単語ベクトルを表示せよ。ただし、”United States”は内部的には”United_States”と表現されていることに注意せよ。
+# Google Newsデータセット（約1,000億単語）での学習済み単語ベクトル（300万単語・フレーズ、300次元）をダウンロードし、"United States"の単語ベクトルを表示せよ。ただし、"United States"は内部的には"United_States"と表現されていることに注意せよ。
 
 from gensim.models import KeyedVectors
+import pandas as pd
 
 # GoogleNewsの単語ベクトルを読み込む
 model = KeyedVectors.load_word2vec_format(
@@ -16,3 +17,9 @@ print(f"ベクトルの次元数: {model.vector_size}")
 word = "United_States"
 if word in model:
     print(word,'の単語ベクトル：',model[word])
+
+# データをDataFrameに変換
+# data = [['love', 'sex', '6.77'], ['tiger', 'cat', '7.35'], ['tiger', 'tiger', '10.00']]  # 例データ
+df = pd.DataFrame(data, columns=['word1', 'word2', 'human'])
+print("\nDataFrame:")
+print(df)
